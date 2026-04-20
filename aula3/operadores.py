08d3035dff51573a33919e8b2d1072b42cb05304
Jadson-Jose <jadsonjosesilva@gmail.com>
2026-04-20 16:14:43 -0300

    aula 3

---
+++ aula3/operadores.py
@@ -0,0 +1,43 @@
+# Operadores básicos
+
+# Adiação (+)
+# print(10 + 5)
+
+# Subtração (-)
+# print(10 - 5)
+
+# multiplicação (*)
+# print(10 * 5)
+
+# Divisão (/)
+# print(10 / 5)
+
+
+# Operadores avançados (muito importante)
+
+# Divisão inteira
+# print(10 // 5)
+
+# Módulo
+# print(2 % 5)
+
+# Potência
+# print(2**3)
+
+
+# Ordem de precedência
+# Assim como na matemática, existe uma ordem de prioridade.
+# print(10 + 5 * 2)
+# print((10 + 5) * 2)
+
+
+# Exemplo prático
+# numero = int(input("Digite um número: "))
+
+# print("Dobro:", numero * 2)
+# print("Triplo:", numero * 3)
+# print("Raiz quadrada:", numero**0.5)
+
+
+# Erro comum
+print("10" + "5")
---
+++ aula3/tipos.py
@@ -0,0 +1,27 @@
+"""idade = 25
+print(type(idade))
+
+altura = 1.75
+print(type(altura))
+
+nome = "Jadson"
+print(type(nome))
+
+ativo = True
+print(type(ativo))
+"""
+
+"""print(type(23))
+print(type("23"))"""
+
+"""nome = input("Digite seu nome: ")
+print(type(nome))"""
+
+"""numero = int(input("Digite um numero: "))
+print(type(numero))
+
+numero2 = float(input("Digite outro numero: "))
+print(type(numero2))"""
+
+nome2 = str(input("Digite o seu nome: "))
+print(type(nome2))
