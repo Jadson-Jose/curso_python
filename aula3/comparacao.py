864ef1a25902f971c85217f0adbad7cff3656b5c
Jadson-Jose <jadsonjosesilva@gmail.com>
2026-04-21 09:13:11 -0300

    comparação

---
+++ aula3/comparacao.py
@@ -0,0 +1,31 @@
+# IGUAL (==)
+# print(10 == 10)
+
+
+# # DIFERENTE (!=)
+# print(10 != 5)
+
+# # MAIOR QUE (>)
+# print(10 > 5)
+
+# # MENOR QUE (<)
+# print(5 < 10)
+
+# # MAIOR OU IGUAL
+# print(10 >= 10)
+
+# # MENOR OU IGUAL
+# print(5 <= 10)
+
+
+# # EXEMPLOS PRÁTICOS
+# idade = 18
+# print(idade >= 20)
+
+
+# # ERRO MUITO COMUM
+# print(10 = 10)
+
+# # EXMPLO REAL (USO PRÁTICO)
+idade = int(input("Digite a sua idade: "))
+print(idade >= 18)
---
+++ aula3/fstrings.py
@@ -0,0 +1,35 @@
+# nome = "Jadson"
+# idade = 40
+
+# print("Meu nome é " + nome + "tenho" + str(idade) + "anos")
+# print(f"Meu nome é {nome} e tenho {idade} anos")
+
+
+# Mais exemplos com fstrings
+# nome = "Maria"
+# idade = 30
+
+# print(f"Olá, meu nome é {nome}")
+# print(f"Eu tenho {idade} anos")
+
+
+# Expressões com fstrings
+# numero = 10
+# print(f"O dobro de {numero} é {numero * 2}")
+
+
+# Formatando números
+# preco = 49.9
+# print(f"Preço: {preco:.2f}")
+
+
+# ERRO comum
+nome = "João"
+# print("meu nome é {nome}")  # Não vai mostrar o valor da variável nessa linha
+# print(f"Meu nome é {nome}")
+
+# Exemplo real
+# nome = input("Digite o seu nome: ")
+# idade = int(input("Digite a sua idade: "))
+
+# print(f"Olá {nome}, você tem {idade} anos.")
